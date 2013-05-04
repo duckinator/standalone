197b75d976e1b8a5b9226255f7f35229b9d735bf
Nick Markwell <nick@duckinator.net>
2013-05-04 13:36:58 -0400

    Add replacement ENV.

---
+++ lib/standalone/env.rb
@@ -0,0 +1,13 @@
+module Standalone
+ ENV = {
+   "LANG"    => "en_US.UTF-8",
+   "SHLVL"   => "1",
+   "PWD"     => "/home/sicuro",
+   "USER"    => "sicuro",
+   "LOGNAME" => "sicuro",
+   "HOME"    => "/home/sicuro",
+   "PATH"    => "/bin",
+   "SHELL"   => "/bin/bash",
+   "TERM"    => "dumb"
+ }
+end
