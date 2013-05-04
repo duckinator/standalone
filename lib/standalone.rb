88a37e6818fb313e3ef6cf353cc234ff8f2d1c9a
Nick Markwell <nick@duckinator.net>
2013-05-04 13:37:44 -0400

    Add Standalone.enable!

--- lib/standalone.rb
+++ lib/standalone.rb
@@ -1,10 +1,20 @@
-require 'standalone/version'
 %w[
+  version
   file
+  env
 ].each do |file|
   require "standalone/#{file}"
 end
 
 module Standalone
-  # Your code goes here...
+  # This removes a constant (to avoid "already initialized constant"), then
+  # defines it to the value specified in Sicuro::Runtime::Constants.
+  def self.enable!
+    self.constants.each do |x|
+      Object.instance_eval do
+        remove_const x if const_defined?(x)
+        const_set(x, Standalone.const_get(x))
+      end
+    end
+  end
 end
